3c4130a952a25df8aa4023caecde2b9eedd38677
Rameez Mughal <rameezhm@outlook.com>
2023-06-19 12:26:40 -0700

    begin adding web scraping capability

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import sys, os, json, openai
+from bs4 import BeautifulSoup
+
+def superprint(s):
+    sys.stdout.buffer.write(s.encode('utf8'))
+    print("")
 
 config = json.load(open('config.json', 'r'))
 openai.api_key = open("OPENAI_KEY").read().strip()
@@ -6,15 +11,23 @@ input_files = os.listdir('input')
 
 for file in input_files:
     print("Reading", file, "...")
+
     chapter_text = open('input/' + file, encoding='utf-8').read()
+    if config['webpage_mode']:
+        soup = BeautifulSoup(chapter_text, 'html.parser')
+        text_element = soup.find(config['wp_element'], {"class": config['wp_class']})
+        chapter_text = text_element.get_text()
+        
 
     print("Sending text to OpenAI ...")
+    prompt = config['prompt']
+
     chat_completion = openai.ChatCompletion.create(
         model = config['model'],
         messages = [
             {
                 "role": "system",
-                "content": config['prompt']
+                "content": prompt
             },
             {
                 "role": "user",
